a3b2fb7c42c65ba14f96c1fa4624fc1c39defbec
Leonid Sviderskii <svidersky.leonid@gmail.com>
2020-07-27 12:07:28 -0400

    Added searchForContent function

--- search-results-validation.js
+++ search-results-validation.js
@@ -1,4 +1,22 @@
 /**
+ * Seaches for a content using provided string
+ * @param  {[Text]} phrase [Search query]
+ * @example
+    import {searchForContent} from './search_utils'
+    searchForContent("phrase")
+ */
+export const searchForContent = (phrase) => {
+   cy.server();
+   cy.route(`**filter=title=@\\*${phrase}\\**`).as('search');
+   cy.get(searchField)
+      .should("be.visible") // Needed check to avoid failures when a dropdown is still covering the field
+      .type(phrase);
+   cy.get(searchButton).click();
+   };
+   cy.wait('@search');
+};
+
+/**
 * Validates Search results on the Search page in case of successful search:
 * @param  {[Text]} queryText [Query that was used to perform search]
 * @example
